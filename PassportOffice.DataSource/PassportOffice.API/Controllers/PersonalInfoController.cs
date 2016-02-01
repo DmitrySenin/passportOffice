188684b470d43a350b4a48b2377d6c03e8a3ec97
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-01 18:49:29 +0300

    DSP-15. Created API. Add controller with API for getting personal information.

---
+++ PassportOffice.DataSource/PassportOffice.API/Controllers/PersonalInfoController.cs
@@ -0,0 +1,18 @@
+﻿namespace PassportOffice.API.Controllers
+{
+    using System.Web.Http;
+
+    using PassportOffice.DataSource.UnitOfWork;
+
+    /// <summary>
+    /// Controller which contains API to access personal information.
+    /// </summary>
+    [RoutePrefix("personalinfo")]
+    public class PersonalInfoController : ApiController
+    {
+        /// <summary>
+        /// Collection of repositories contain information from database.
+        /// </summary>
+        private UnitOfWork databaseRepos = new UnitOfWork();
+    }
+}
