2d62614a1081dbffb781cf785890406fb94cba23
dmitry.senin <white.lord.mane@gmail.com>
2016-02-04 13:21:33 +0400

    DSP-17. Display given information. Add controller to bind scope's variable with loaded data.

---
+++ PassportOffice.DataSource/PassportOffice.View/app/PersonalData/PersonalInfoCtrl.js
@@ -0,0 +1,19 @@
+;angular.module('main').controller("PersonalInfoCtrl", ['$scope', 'PersonalInfoLoader',
+	function($scope, PersonalInfoLoader) {
+
+	getInfo();
+
+	function getInfo() {
+		PersonalInfoLoader.Load()
+			.success(function(data) {
+				$scope.PersonalInfo = data;
+				$scope.PersonalInfo.map(function(item, index, arr) {
+					arr[index].BirthdayDate = new Date(item.BirthdayDate);
+					arr[index].PassportIssueDate = new Date(item.PassportIssueDate);
+				});
+			})
+			.error(function(error) {
+				$scope.error = error;
+			});
+	}
+}]);
