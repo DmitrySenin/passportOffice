83bb8c1fca5ac8b7f659bcb5528b6055f8bce811
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-05 16:52:48 +0300

    DSP-19. DB searching. Add methods to check what options should be used for searching.

--- PassportOffice.DataSource/PassportOffice.DataSource/Searching/PersonalInfoSearchingOptions.cs
+++ PassportOffice.DataSource/PassportOffice.DataSource/Searching/PersonalInfoSearchingOptions.cs
@@ -36,5 +36,59 @@
         /// Restriction of person's date of birth.
         /// </summary>
         public DateTime BirthdayDate { get; set; }
+
+        /// <summary>
+        /// Checks that first name should be used for searching.
+        /// </summary>
+        /// <returns>Flag which identifies that option should be used.</returns>
+        public bool UseFirstName()
+        {
+            return !string.IsNullOrEmpty(this.FirstName);
+        }
+
+        /// <summary>
+        /// Checks that last name should be used for searching.
+        /// </summary>
+        /// <returns>Flag which identifies that option should be used.</returns>
+        public bool UseLastName()
+        {
+            return !string.IsNullOrEmpty(this.LastName);
+        }
+
+        /// <summary>
+        /// Checks that middle name should be used for searching.
+        /// </summary>
+        /// <returns>Flag which identifies that option should be used.</returns>
+        public bool UseMiddleName()
+        {
+            return !string.IsNullOrEmpty(this.MiddleName);
+        }
+
+        /// <summary>
+        /// Checks that series of passport should be used for searching.
+        /// </summary>
+        /// <returns>Flag which identifies that option should be used.</returns>
+        public bool UsePassportSeries()
+        {
+            return !string.IsNullOrEmpty(this.PassportSeries);
+        }
+
+        /// <summary>
+        /// Checks that number of passport should be used for searching.
+        /// </summary>
+        /// <returns>Flag which identifies that option should be used.</returns>
+        public bool UsePassportNumber()
+        {
+            return !string.IsNullOrEmpty(this.PassportNumber);
+        }
+
+        /// <summary>
+        /// Checks that date of birthday should be used for searching.
+        /// </summary>
+        /// <returns>Flag which identifies that option should be used.</returns>
+        public bool UseBirthdayDate()
+        {
+            return this.BirthdayDate != null;
+        }
     }
 }
