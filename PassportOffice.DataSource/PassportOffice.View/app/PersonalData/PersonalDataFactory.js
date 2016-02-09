6773a2ec5281092c31da9ac33781fa44c96a7127
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-09 11:22:45 +0300

    DSP-25. Client side paging. Refactor personal data factory.

--- PassportOffice.DataSource/PassportOffice.View/app/PersonalData/PersonalDataFactory.js
+++ PassportOffice.DataSource/PassportOffice.View/app/PersonalData/PersonalDataFactory.js
@@ -1,42 +1,38 @@
-;angular.module('main', ['ui.bootstrap']).factory("PersonalInfoLoader", ['$http', function ($http) {
-	
-	/**
-	 * URL of server.
-	 * @type {String}
-	 */
-	var BASE_URL = "http://localhost:57336/";
+;(function() {
+	angular
+		.module('main')
+		.factory("PersonalInfoLoader", PersonalInfoLoader);
 
-	/**
-	 * Create URL to API to getting passport data.
-	 * @return {string} URL for requesting personal data from server. 
-	 */
-	function buildPersonalInfoGetURL() {
-		return BASE_URL + "personalinfo";
-	}
+	PersonalInfoLoader.$inject = ['$http', 'URLService'];
 
-	var PersonalInfoLoader = {};
+	function PersonalInfoLoader($http, URLService) {
 
-	/**
-	 * Create instance of object represents searching criteria.
-	 * @return {object} Object which fields contatins restirction of searching.
-	 */
-	PersonalInfoLoader.SearchingOptions = function () {
-		this.FirstName = null;
-		this.LastName = null;
-		this.MiddleName = null;
-		this.BirthdayDate = null;
-		this.PassportSeries = null;
-		this.PassportNumber = null;
+		return {
+			SearchingOptions : SearchingOptions,
+			Load : Load
+		};
+		
+		/**
+		 * Create instance of object represents searching criteria.
+		 * @return {Object} Object which fields contatins restirction of searching.
+		 */
+		function SearchingOptions() {
+			this.FirstName = null;
+			this.LastName = null;
+			this.MiddleName = null;
+			this.BirthdayDate = null;
+			this.PassportSeries = null;
+			this.PassportNumber = null;
+		};
+
+		/**
+		 * Carries out request for getting information from passed URL.
+		 * @param {Numeric} pageSize Count of records on one page.
+		 * @param {String} pageNumber Number 
+		 * @return {Promise}     Promise which represents flow of request.
+		 */
+		function Load(pageSize, pageNumber, searchingOptions) {
+			return $http.get(URLService.BuildPersonalInfoGetURL(pageSize, pageNumber), { params : searchingOptions });
+		};
 	};
-
-	/**
-	 * Carries out request for getting information from passed URL.
-	 * @param  {string} URL Target URL of request.
-	 * @return {Promise}     Promise which represents flow of request.
-	 */
-	PersonalInfoLoader.Load = function(searchingOptions) {
-		return $http.get(buildPersonalInfoGetURL(), { params : searchingOptions });
-	};
-
-	return PersonalInfoLoader;
-}]);
+})();
