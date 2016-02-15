e28da2dd623c9a918000234359d6c13e76ac7115
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-15 17:24:21 +0300

    DSP-33. Full sorting server. Change implementation of manager to not sort data by all fields by default.

--- PassportOffice.DataSource/PassportOffice.API/DataAccess/PersonalInfoManager.cs
+++ PassportOffice.DataSource/PassportOffice.API/DataAccess/PersonalInfoManager.cs
@@ -27,20 +27,22 @@
         /// <summary>
         /// Finds all records of personal information.
         /// </summary>
+        /// <param name="fullSort">Flag which identifies what fields should be used to sord data.</param>
         /// <returns>All passport data.</returns>
-        public IEnumerable<PersonInfo> GetAll()
+        public IEnumerable<PersonInfo> GetAll(bool fullSort = false)
         {
-            return this.reposCollection.PersonalInfoRepository.GetAll();
+            return this.reposCollection.PersonalInfoRepository.GetAll(fullSort);
         }
 
         /// <summary>
         /// Finds all records of personal data using for selection searching criteria.
         /// </summary>
         /// <param name="searchingOptions">Restriction which used to select data.</param>
+        /// <param name="fullSort">Flag which identifies what fields should be used to sord data.</param>
         /// <returns>All passport data satisfy to searching criteria.</returns>
-        public IEnumerable<PersonInfo> SearchAll(PersonalInfoSearchingOptions searchingOptions)
+        public IEnumerable<PersonInfo> SearchAll(PersonalInfoSearchingOptions searchingOptions, bool fullSort = false)
         {
-            return this.reposCollection.PersonalInfoRepository.SearchAll(searchingOptions);
+            return this.reposCollection.PersonalInfoRepository.SearchAll(searchingOptions, fullSort);
         }
 
         /// <summary>
@@ -49,10 +51,11 @@
         /// <param name="pageSize">Count of records on one page.</param>
         /// <param name="pageNumber">Number of requested page.</param>
         /// <param name="searchingOptions">Criteria for selection of records.</param>
+        /// <param name="fullSort">Flag which identifies what fields should be used to sord data.</param>
         /// <returns>Records located on requested page.</returns>
-        public IEnumerable<PersonInfo> GetPagedList(int pageSize, int pageNumber, PersonalInfoSearchingOptions searchingOptions)
+        public IEnumerable<PersonInfo> GetPagedList(int pageSize, int pageNumber, PersonalInfoSearchingOptions searchingOptions, bool fullSort = false)
         {
-            return this.reposCollection.PersonalInfoRepository.GetPage(pageSize, pageNumber, searchingOptions);
+            return this.reposCollection.PersonalInfoRepository.GetPage(pageSize, pageNumber, searchingOptions, fullSort);
         }
     }
 }
