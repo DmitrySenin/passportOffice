087db544425ef9f987b42f2a134c99029ebee67a
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-16 14:05:55 +0300

    DSP-34. Searching panel. Add datepicker to choose birthday date.

--- PassportOffice.DataSource/PassportOffice.View/app/Search/SearchCtrl.js
+++ PassportOffice.DataSource/PassportOffice.View/app/Search/SearchCtrl.js
@@ -11,5 +11,15 @@
 		var vm = this;
 
 		vm.SearchingOptions = new PersonalInfoLoader.SearchingOptions();
+
+		// Represents datepicker's state.
+		vm.dateprickerPopup = {
+			opened: false
+		};
+
+		// Open datepicker.
+		vm.openDatepickerPopup = function() {
+			vm.dateprickerPopup.opened = true;
+		};
 	}
 })();
