750561c2c3ebe760fdf723c749c6c84000a94089
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-17 18:36:26 +0300

    DSP-30. Client side authentication. Add usage of authentication service to controller to allow user login and logout.

--- PassportOffice.DataSource/PassportOffice.View/app/Users/UsersCtrl.js
+++ PassportOffice.DataSource/PassportOffice.View/app/Users/UsersCtrl.js
@@ -5,18 +5,69 @@
 		.module('main')
 		.controller('UsersCtrl', UsersCtrl);
 
-	function UsersCtrl() {
+	UsersCtrl.$inject = ['AuthService'];
+
+	function UsersCtrl(AuthService) {
 		var vm = this;
 
+		catchAuthenticationFlag(vm);
 		createCredentialsFields(vm);
+		setAuthSuccessFlag(vm, true);
+
+		/**
+		 * Try to login user with entered credentials.
+		 */
+		vm.Login = function() {
+			AuthService.Login(vm.username, vm.password).then(function(data) {
+				catchAuthenticationFlag(vm);
+				hideLoginForm();
+				setAuthSuccessFlag(vm, true);
+			}, 
+			function(error) {
+				catchAuthenticationFlag(vm);
+				setAuthSuccessFlag(vm, false);
+			});
+		};
+
+		/**
+		 * Logout current user.
+		 */
+		vm.Logout = function() {
+			AuthService.Logout();
+			catchAuthenticationFlag(vm);
+		};
 
 		/**
 		 * Add variables to store credentials to scope.
-		 * @return {Object} Object where credentials should be added.
+		 * @param {Object} scope Object where credentials should be added.
 		 */
 		function createCredentialsFields(scope) {
 			scope.username = '';
 			scope.password = '';
+		}
+
+		/**
+		 * Get flag of user authentication from service and set value to scope.
+		 * @param {Object} scope Object where credentials should be added.
+		 */
+		function catchAuthenticationFlag(scope) {
+			vm.IsAuthenticated = AuthService.IsAuthenticated;
+		}
+
+		/**
+		 * Set login form invisible.
+		 */
+		function hideLoginForm() {
+			$('.dropdown').removeClass('open');
+		}
+
+		/**
+		 * Set value of flag that represent successful authentication.
+		 * @param {Object} scope Object with flag.
+		 * @param {Boolean} value New value of flag.
+		 */	
+		function setAuthSuccessFlag(scope, value) {
+			scope.IsAuthSuccess = !!value;
 		}
 	}
 
