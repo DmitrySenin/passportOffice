ff0b2217b9489bb534940e44de2bf7847e3c56d0
dmitry.senin <dmitry.senin@akvelon.com>
2016-02-18 17:19:39 +0300

    DSP_37. Users API. Create controller which manages request about users.

---
+++ PassportOffice.DataSource/PassportOffice.API/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿namespace PassportOffice.API.Controllers
+{
+    using System.Security.Claims;
+    using System.Web.Http;
+
+    using PassportOffice.API.DataAccess;
+    using PassportOffice.API.Utility;
+
+    /// <summary>
+    /// Manages information about users.
+    /// </summary>
+    [RoutePrefix("users")]
+    public class UsersController : ApiController
+    { }
+}
